190906363831213ec163cca077c804b7c514f939
developerkunal <dawar.kunal703@gmail.com>
2023-04-17 21:35:20 +0530

    FIx DOcker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     loader: 'akamai',
     path: '',  },
